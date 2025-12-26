ed745be8a1a11fd8dbea31e191c6ae3b380d54f6
Aatir <aatirali18@gmail.com>
2025-12-26 23:06:31 +0530

    ILL fix one of the big bug

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -17,15 +17,16 @@ from xgboost import XGBRegressor
 from src.exception import CustomException
 from src.logger import logging
 
-from src.utils import save_object ,evaluate_models
+from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
     trained_model_file_path=os.path.join("artifacts","model.pkl")
 
-    class ModelTrainer:
-        def __init__(self):
-            self.model_trainer_config=ModelTrainerConfig()
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+
 
     def initiate_model_trainer(self,train_array,test_array):
         try:
@@ -46,9 +47,8 @@ class ModelTrainerConfig:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
             
-
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test, 
-                                              models=models)
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models)
             
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
@@ -68,12 +68,15 @@ class ModelTrainerConfig:
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
             )
-            
 
             predicted=best_model.predict(X_test)
 
             r2_square = r2_score(y_test, predicted)
             return r2_square
+            
 
+
+
+            
         except Exception as e:
             raise CustomException(e,sys)
